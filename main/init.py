bad1c747032de6eaf0c2b350511180efd4cfc734
Schmuel <dev@schmuel.net>
2024-05-17 02:54:10 +0200

    Nicht genutztes Modul entfernt

--- main/init.py
+++ main/init.py
@@ -2,10 +2,10 @@
 """
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 import os
+import shutil
 
 
 
